f9419989f106f7fbd03ccb5e7bb886248db7a8a5
PK Banks <pkbanks12@gmail.com>
2018-05-05 15:52:23 -0400

    new component

---
+++ src/components/StorePicker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+
+  render() {
+    return <p>Hello!, from StorePicker!</p>
+  }
+
+}
+
+export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-// let's go!
-alert('hey');
+import React from 'react';
+import { render } from 'react-dom'
+import StorePicker from './components/StorePicker.js'
+
+// render(<p>Heyyyy</p>, document.querySelector('#main'));
+render(<StorePicker />, document.querySelector('#main'));
