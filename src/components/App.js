8d0da05595de1f621158fd66998a628976019155
PK Banks <pkbanks12@gmail.com>
2018-05-05 17:35:15 -0400

    props

--- src/components/App.js
+++ src/components/App.js
@@ -10,10 +10,10 @@ class App extends React.Component {
     return(
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh Seafood Market" />
+        </div>
           <Inventory />
           <Order />
-        </div>
       </div>
     )
   }
