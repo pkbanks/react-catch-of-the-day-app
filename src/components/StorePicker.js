b5086ce177af16c5606ce00d0f12a7f0df0b4607
PK Banks <pkbanks12@gmail.com>
2018-05-07 08:41:06 -0400

    reading input value, for form submission

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,11 +3,23 @@ import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
 
+  goToStore = (event) => {
+    // Stop form from submitting
+    event.preventDefault();
+
+    // Get text from input
+    console.log(this);
+
+    // Change page to /store/:id
+    console.log("Going to store");
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         { /* this is a comment */ }
         <h2>Please Enter a Store</h2>
+
         <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store -></button>
       </form>
