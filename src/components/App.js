8987b1ee21ce4878939bce788aa3cafb1e0126d4
PK Banks <pkbanks12@gmail.com>
2018-05-07 14:01:53 -0400

    able to alter state through button in child component

--- src/components/App.js
+++ src/components/App.js
@@ -26,13 +26,22 @@ class App extends React.Component {
     this.setState({ fishes: sampleFishes });
   }
 
+  addToOrder = (key) => {
+    // take a copy of state
+    const order = {...this.state.order}
+    // either add to the order, or update the quantity
+    order[key] = order[key] + 1 || 1;
+    // call setState to update
+    this.setState({ order });
+  }
+
   render() {
     return(
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)}
           </ul>
         </div>
           <Order />
