9293a01ef75743b04482c6239fcc4938703a760b
PK Banks <pkbanks12@gmail.com>
2018-05-08 22:29:12 -0400

    firebase implementation

---
+++ src/base.js
@@ -0,0 +1,14 @@
+import Rebase from 're-base';
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+  // source: firebase site
+    apiKey: "AIzaSyAJmmsUEgwOqGtjibdici1GK8xq8cHwUtI",
+    authDomain: "catch-of-the-day-pk-banks.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-pk-banks.firebaseio.com"
+})
+
+const base = Rebase.createClass(firebaseapp.database());
+
+// this is a named export
+export { firebaseApp };
