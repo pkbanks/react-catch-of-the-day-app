ecea8d1b3c08b98c07baa9d502e5ab495af218f5
PK Banks <pkbanks12@gmail.com>
2018-05-18 09:05:20 -0400

    can click login buttons for auth

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import firebase from 'firebase';
 import AddFishForm from './AddFishForm';
 import EditFishForm from './EditFishForm';
 import Login from './Login';
+import { firebaseApp } from "../base";
+
 
 class Inventory extends React.Component {
 
@@ -13,9 +16,14 @@ class Inventory extends React.Component {
     loadSampleFishes: PropTypes.func
   }
 
-  authenticate = provider => {
-    alert('authenticate');
-  }
+  authHandler = async authData => {
+    console.log(authData);
+  };
+
+  authenticate = (provider) => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebaseApp.auth().signInWithPopup(authProvider).then(this.authHandler);
+  };
 
   render() {
     return <Login authenticate={this.authenticate} />
